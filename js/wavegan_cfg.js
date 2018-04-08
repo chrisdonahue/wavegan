c928d2d73c3ec541011ad35ff22b3d07df1bb238
Chris Donahue <christopherdonahue@gmail.com>
2018-04-08 13:35:20 -0700

    Redirected ckpts to bulk repo

--- js/wavegan_cfg.js
+++ js/wavegan_cfg.js
@@ -11,7 +11,7 @@ window.wavegan = window.wavegan || {};
             mobileWarning: 'Warning: This demo runs a neural network in your browser. It appears you are on a mobile device. Consider running the demo on your laptop instead. Continue?'
         },
         net: {
-            ckptDir: 'ckpts/drums',
+            ckptDir: 'https://chrisdonahue.github.io/wavegan_demo_bulk/ckpts/drums',
             ppFilt: true,
 	    zDim: 100
         },
